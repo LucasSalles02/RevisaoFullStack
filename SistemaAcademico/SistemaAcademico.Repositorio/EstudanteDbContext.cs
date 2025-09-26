2b1b68b91a4a1bfc3da9d7a5d0b2a9589b087ea7
Pedro Lucas Salles <sallespedrolucas380@gmail.com>
2025-09-26 18:16:06 -0300

    Revisao

---
+++ SistemaAcademico/SistemaAcademico.Repositorio/EstudanteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaAcademico.Dominio;
+
+namespace SistemaAcademico.Repositorio
+{
+    public class EstudanteDbContext : DbContext
+    {
+        public EstudanteDbContext(DbContextOptions<EstudanteDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
---
+++ SistemaAcademico/SistemaAcademico.Repositorio/IAlunoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaAcademico.Dominio;
+
+namespace SistemaAcademico.Repositorio
+{
+    public interface IAlunoRepositorio
+    {
+        void Adicionar(Aluno aluno);
+        List<Aluno> Listar();
+    }
+}
